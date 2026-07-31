8118ca6196af088fcfc79657179e988597d30f18
Julia582 <72489746+Julia582@users.noreply.github.com>
2026-07-31 22:33:23 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.54%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,19 +1,3 @@
-class Solution(object):
-    def reverseWords(self, s):
-        result =[]
-        i = len(s)-1
-        while i>=0:
-            while i>=0 and s[i]==" ":
-                i-=1
-            if i<0:
-                break
-            j=i
-            while i>=0 and s[i] !=" ":
-                i-=1
-            result.append(s[i+1:j+1])
-        return " ".join(result)
-
-    
-     
-
-     
+class Solution:
+    def reverseWords(self, s: str) -> str:
+       return " ".join(s.split()[::-1])
